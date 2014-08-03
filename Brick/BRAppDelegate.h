03a8eb5e50746429099f62773ceaa859e7976adc
Evan Coleman <evan@evancoleman.net>
2014-08-02 22:23:58 -0400

    Initial Commit

---
+++ Brick/BRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BRAppDelegate.h
+//  Brick
+//
+//  Created by Evan Coleman on 8/2/14.
+//  Copyright (c) 2014 Evan Coleman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Brick/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Brick
+//
+//  Created by Evan Coleman on 8/2/14.
+//  Copyright (c) 2014 Evan Coleman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BRAppDelegate class]));
+    }
+}
